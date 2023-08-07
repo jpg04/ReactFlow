77eea3a83e3185328e058d99bf6fd4493fa6d83a
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-08-07 17:10:26 +0530

    Understanding React Flow Library | Background & Background Type, Controls |

--- first-react-flow-app/src/App.js
+++ first-react-flow-app/src/App.js
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import ReactFlow, {Background, Controls}  from 'reactflow';
 import 'reactflow/dist/style.css';
 import './App.css';
 
 function App() {
+  const [variant, setVariant] = useState('lines');
   return (
     <>
     <ReactFlow>
-      <Background/>
+      <Background variant={variant} color={'green'} gap={'50'}/>
       <Controls/>
     </ReactFlow>
     </>
