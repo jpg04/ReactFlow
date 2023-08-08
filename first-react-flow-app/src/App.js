070137f438346bcbbaff310a7f35848d1978a025
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-08-08 18:10:21 +0530

    Understanding the concept of React Flow Library | First node and edge react flow

--- first-react-flow-app/src/App.js
+++ first-react-flow-app/src/App.js
@@ -3,7 +3,25 @@ import ReactFlow, {Background, Controls}  from 'reactflow';
 import 'reactflow/dist/style.css';
 import './App.css';
 
+const initialNodes = [
+  {
+    id: '1',
+    data: { label: 'Hello' },
+    position: { x: 0, y: 0 },
+    type: 'input',
+  },
+  {
+    id: '2',
+    data: { label: 'World' },
+    position: { x: 100, y: 100 },
+  },
+];
+
+const initialEdges = [{ id: '1-2', source: '1', target: '2', label: 'to the', type: 'step' }];
+
 function App() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
   const [variant, setVariant] = useState('lines');
   return (
     <>
